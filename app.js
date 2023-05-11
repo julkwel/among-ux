b109170e2fa1bdab0a199761e09c451d14b70254
Fullfifax <fullfifax1245@gmail.com>
2023-05-11 17:05:08 +0000

    add feature that enables user to draw by clicking their mouse

--- app.js
+++ app.js
@@ -4,3 +4,26 @@ document.addEventListener('mouseover',(e)=>{
     mouse.style.top = `${e.clientY}px`;
     console.log(e.clientX,e.clientX);
 });
+
+const board = document.getElementById("board");
+
+let isDrawing = false;
+
+board.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+
+board.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+board.addEventListener("mousemove", event => {
+  if (isDrawing) {
+    const circle = document.createElement("div");
+    circle.classList.add("circle");
+    circle.style.left = event.clientX + "px";
+    circle.style.top = event.clientY + "px";
+    board.appendChild(circle);
+  }
+});
+
