03405cb963fe07296e8c402291baeed855d2ec05
Ravaka15 <ravakiniainatokyhary@gmail.com>
2023-05-16 16:47:25 +0300

    Bouton up

--- app.js
+++ app.js
@@ -35,3 +35,19 @@ playButton.addEventListener("click", function() {
 	audio.play();
 });
 
+const scrollToTopButton = document.getElementById("scrollToTopButton");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+});
+
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
