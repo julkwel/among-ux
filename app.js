5797b4fee59e2babdf93d8b82fb9f163018dd4a9
Selestino <selestinoolivier@gmail.com>
2023-05-18 14:24:28 +0300

    enleve mouse

--- app.js
+++
@@ -1,6 +0,0 @@
-let mouse = document.querySelector('#mouse');
-document.addEventListener('mouseover',(e)=>{
-    mouse.style.left = `${e.clientX+2}px`;
-    mouse.style.top = `${e.clientY}px`;
-    console.log(e.clientX,e.clientX);
-});
