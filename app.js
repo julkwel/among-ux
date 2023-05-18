c780dca2b66f60fc3941896b52ea78eb0676e5a2
Selestino <selestinoolivier@gmail.com>
2023-05-19 00:21:06 +0300

    add buttons with translate random

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ const setUpCamera = () => {
     }
 }
 setUpCamera();
+
+let translats = [40, -40];
+let inputs = document.querySelectorAll('input');
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('mouseover', function (e) {
+        console.log(e.target);
+      let translat1 = Math.floor(Math.random()*10)%2==0?translats[0]:translats[1];
+      let translat2 = Math.floor(Math.random()*10)%2==0?translats[0]:translats[1];
+      e.target.style.transform = `translateX(${translat1}px) translateY(${translat2}px)`;
+    });
+    inputs[i].addEventListener('mouseout', function (e) {
+        e.target.style.transform = `none`;
+    });
+  }
