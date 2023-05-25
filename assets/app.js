2f0fa127509565249bf85c1f48f0adc634523e0a
Selestino <selestinoolivier@gmail.com>
2023-05-25 18:23:03 +0300

    adding header && link's title strange

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,9 @@ const lastUpdateContainer = document.getElementById('assets-last-update-containe
 const viewLastUpdate = document.getElementById('view-the-last-update');
 const quitTheLastUpdate = document.getElementById('quit-the-last-update');
 const viewTheMainContainer = document.getElementById('view-redirect-url');
+const pLink = document.querySelectorAll('.link p');
+const textIn = ['Update','About','Home'];
+const textOut = ['Home','Update','About'];
 
 viewLastUpdate.onclick = function() {
   lastUpdateContainer.style.display = 'flex';
@@ -10,4 +13,14 @@ viewLastUpdate.onclick = function() {
 
 quitTheLastUpdate.onclick = function () {
   window.location.reload();
+}
+
+for (let i = 0; i < pLink.length; i++) {
+  pLink[i].onmouseenter = function (e) {
+    e.target.innerText = textIn[i];
+    e.target.setAttribute('title',`Oh sorry, this is the ${textIn[i]}`);
+  };
+  pLink[i].onmouseout = function (e) {
+    e.target.innerHTML = textOut[i];
+  };
 }
